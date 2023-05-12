6230554b72b6f3c640f65042cf392bf9e68f644f
ayda12345 <123007630+ayda12345@users.noreply.github.com>
2023-05-12 13:49:37 +0300

    Add files via upload

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -20,16 +20,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'foodmine-course'`, () => {
+  it(`should have as title 'todoapp'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('foodmine-course');
+    expect(app.title).toEqual('todoapp');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('.content span')?.textContent).toContain('foodmine-course app is running!');
+    expect(compiled.querySelector('.content span')?.textContent).toContain('todoapp app is running!');
   });
 });
